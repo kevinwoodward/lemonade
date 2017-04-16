634f85d117ab613c658bc94c8e308e764bbf428a
Amit Khatri <amit.vkhatri@gmail.com>
2017-04-16 03:57:21 -0700

    Added a couple of comments

--- boilerplateMPD.c
+++ boilerplateMPD.c
@@ -12,14 +12,15 @@
 int main()
 {
   struct mpd_connection *conn;
+  //Create a new connection at the specified address
   conn = mpd_connection_new("127.0.0.1",0,30000);
 
-  if(mpd_connection_get_error(conn) != MPD_ERROR_SUCCESS){
+  //Error check
+  if(mpd_connection_get_error(conn) != MPD_ERROR_SUCCESS)
+  {
     printf("Connection Failed!\n");
   }
-  else{
-    printf("Connection Success!\n");
-  }
+
 
   return 0;
 }
