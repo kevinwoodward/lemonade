44eb964753116e8e3f367a075fedf3b275b4c179
kevinwoodward <woodwardk96@hotmail.com>
2017-04-21 12:47:45 -0700

    basic playback, file choose, play/pause, kill added. Must change directory path in code (line 78 at time of commit)

--- client.c
+++ client.c
@@ -4,83 +4,83 @@
 #include <ctype.h>
 #include <unistd.h>
 
-void playpause();
-void startProcess();
-void loadSongFile();
-char* getPidString();
-
-int main(int argc, char **argv)
-{
-
-  /*int pflag = 0;
-  int fileflag = 0;
-  char *cvalue = NULL;
-  int index;
-  int c;
-
-  opterr = 0;
-
-  while ((c = getopt (argc, argv, "pf:")) != -1) {
-    switch (c)
-      {
-      case 'p':
-        playpause();
-        printf("%s\n", "here we want to play/pause currently playing song");
-        break;
-      case 'f':
-        cvalue = optarg;
-        printf("%s\n", "here we want to take a filename as a param and play it");
-        break;
-      case '?':
-        if (optopt == 'f')
-          fprintf (stdout, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stdout, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stdout,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return 1;
-      default:
-        abort ();
-      }
-    }*/
-  startProcess();
-  loadSongFile();
-  return 0;
-
+void createScreen() {
+  system("screen -d -m -S lemonade");
 }
 
-void playpause() {
-  /*system("cat | mpg123 *.mp3");
-  int pid = system("pidof mplayer");
-  char pidstr[10];
-  sprintf(pidstr, "%d", pid);
-  char str[150];
-  strcpy(str, "echo -n p > /proc/");
-  strcat(str, pidstr);
-  strcat(str, "/cd/0");
-  sprintf(str, "%d", pid);
-  system(str);*/
+void sendScreenCommand(char* command) {
+  char str[300];
+  strcpy(str, "screen -S lemonade -X stuff '");
+  strcat(str, command);
+  strcat(str, "^M'");
+  char buffer[500];
+  snprintf(
+    buffer,
+    sizeof(buffer),
+    "screen -S lemonade -X stuff '%s^M'",
+    command
+  );
+  system(buffer);
   return;
 }
 
-void startProcess() {
-  system("cat | mpg123 -R");
+void playpause() {
+  system("screen -S lemonade -X stuff ' ^M'");
+  return;
 }
 
-void loadSongFile() {
-  char str[150];
-  char* pidStr = getPidString();
-  strcpy(str, "echo \"load em.mp3\" > /proc/");
-  strcat(str, pidStr);
-  strcat(str, "/fd/0");
-  system(str);
-}
+int main(int argc, char **argv) {
 
-char* getPidString() {
-  int procID = system("pidof mpg123");
-  static char pidStr[10];
-  sprintf(pidStr, "%d", procID);
-  return pidStr;
+  if(argc <= 1) {
+    printf("No option specified! Currently: -s for start, -p for play/pause, -k for kill.\n");
+  }
+
+  int argCase;
+  char* filePath = NULL;
+
+
+  while((argCase = getopt(argc, argv, "ps:k")) != -1) {
+    switch (argCase) {
+      case 'p':
+        playpause();
+        return 0;
+        break;
+      case 's':
+        filePath = optarg;
+        break;
+      case 'k':
+        system("killall screen");
+        return 0;
+        break;
+      case '?':
+        if (optopt == 's') {
+          fprintf (stdout, "Option -%c requires an argument.\n", optopt);
+          return 1;
+        }
+        else if (isprint (optopt)) {
+          fprintf (stdout, "Unknown option `-%c'.\n", optopt);
+          return 1;
+        }
+        else {
+          fprintf (stdout,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+                   return 1;
+        }
+      default:
+        printf("no\n");
+    }
+  }
+
+  if(filePath != NULL) {
+    system("killall screen");
+    createScreen();
+    sendScreenCommand("cd ~/Documents/github/lemonade");
+    char fileStr[100];
+    strcpy(fileStr, "mpg123 -C ");
+    strcat(fileStr, filePath);
+    sendScreenCommand(fileStr);
+  }
+
+  return 0;
 }
