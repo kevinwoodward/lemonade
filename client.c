ac6cc8d765366a8eb0e3e2d7468ca17f590c7736
kevinwoodward <woodwardk96@hotmail.com>
2017-04-28 09:49:08 -0700

    added some comments and structure for handling ls output to backend

--- client.c
+++ client.c
@@ -5,10 +5,12 @@
 #include <unistd.h>
 
 void createScreen() {
+  //creates a new detachted screen terminal instance with the name lemonade
   system("screen -d -m -S lemonade");
 }
 
 void sendScreenCommand(char* command) {
+  //takes the passed string and shoves it into the previously opened screen and auto executes it within said screen
   char str[300];
   strcpy(str, "screen -S lemonade -X stuff '");
   strcat(str, command);
@@ -25,21 +27,34 @@ void sendScreenCommand(char* command) {
 }
 
 void playpause() {
-  system("screen -S lemonade -X stuff ' ^M'");
+  //send the space char to the screen, causing a pause or play depending on current state.
+  sendScreenCommand(" ");
   return;
+}
+
+void testList() {
+  //in progress for getting ls output and handling accordingly
+  FILE *ls = popen("ls --file-type", "r");
+  char buf[256];
+  while (fgets(buf, sizeof(buf), ls) != 0) {
+    //handle the contents of the ls here, including file vs dir, playback, cding, etc
+    printf("%s", buf);
+  }
+  pclose(ls);
+
 }
 
 int main(int argc, char **argv) {
 
   if(argc <= 1) {
-    printf("No option specified! Currently: -s for start, -p for play/pause, -k for kill.\n");
+    printf("No option specified! Currently:\n-s for start, followed by a filepath to an mp3\n-p for play/pause\n-k for kill\n-e to enter screen\n-l to do the test ls function\n");
   }
 
   int argCase;
   char* filePath = NULL;
 
-
-  while((argCase = getopt(argc, argv, "ps:ke")) != -1) {
+  //handles flag options
+  while((argCase = getopt(argc, argv, "ps:kel")) != -1) {
     switch (argCase) {
       case 'p':
         playpause();
@@ -54,6 +69,10 @@ int main(int argc, char **argv) {
         break;
       case 'e':
         system("screen -r");
+        break;
+      case 'l':
+        testList();
+        break;
       case '?':
         if (optopt == 's') {
           fprintf (stdout, "Option -%c requires an argument.\n", optopt);
@@ -69,11 +88,10 @@ int main(int argc, char **argv) {
                    optopt);
                    return 1;
         }
-      default:
-        printf("no\n");
     }
   }
 
+  //starts process if command is -s with specified song
   if(filePath != NULL) {
     system("killall screen");
     createScreen();
