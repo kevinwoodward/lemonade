72692e10982b13f971602545a11cee317b33a005
reschouw <rybluemip@gmail.com>
2017-05-10 08:25:49 -0700

    Added explanatory comments.

--- winInfo.h
+++ winInfo.h
@@ -2,6 +2,12 @@
 //CMPS 115 Spring 2017
 //Lemonade music player
 
+//	This is a simple organizer class used to simplify
+//	the way window creation and destruction is handled.
+//	Holds pointers to a WINDOW, MENU, and 2d array of ITEMS
+//	as well as a number of items in the menu.
+
+
 #ifndef WININFO_H
 #define WININFO_H
 
